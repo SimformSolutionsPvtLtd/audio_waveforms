7b975145d690e8876b73695de92f0045e45b4d41
Talha Javed Khan <50271102+talhakhan1297@users.noreply.github.com>
2024-11-17 15:10:00 +0500

    Update SwiftAudioWaveformsPlugin.swift (#359)
    * Update SwiftAudioWaveformsPlugin.swift
    
    Removed random back tick before flutter import

--- ios/Classes/SwiftAudioWaveformsPlugin.swift
+++ ios/Classes/SwiftAudioWaveformsPlugin.swift
@@ -1,4 +1,4 @@
-`import Flutter
+import Flutter
 import UIKit
 
 public class SwiftAudioWaveformsPlugin: NSObject, FlutterPlugin {
@@ -178,4 +178,4 @@ public class SwiftAudioWaveformsPlugin: NSObject, FlutterPlugin {
         }
     }
 }
-`
+
