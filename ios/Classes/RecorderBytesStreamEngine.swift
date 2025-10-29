9a54a09b1b0f0f7282d08efde8ee1981f3a4bf73
dhyash-simform <yash.dhrangdhar@simformsolutions.com>
2025-10-29 18:11:19 +0530

    fix: :bug: Fix an issue where recording audio caused an exception when running in a loop - Fix issue where audio couldn’t be recorded on the second attempt

--- ios/Classes/RecorderBytesStreamEngine.swift
+++ ios/Classes/RecorderBytesStreamEngine.swift
@@ -46,7 +46,6 @@ class RecorderBytesStreamEngine {
         var audioSamples = [Float32](repeating: 0.0, count: frameLength)
         var sumOfSquares: Float = 0.0
 
-
         for i in 0..<frameLength {
             audioSamples[i] = channelData[i]
             sumOfSquares += channelData[i] * channelData[i]
@@ -58,7 +57,7 @@ class RecorderBytesStreamEngine {
             audioSamples, 1, &rms,
             vDSP_Length(frameLength)
         )
-        
+
         // Normalize RMS to 0-1 range (assuming max amplitude is 1.0 for Float32)
         let normalizedRms = Double(min(rms, 1.0))
 
